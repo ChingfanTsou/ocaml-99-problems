621db6bbdcdc2bb700cd147c57915ad716e8a7fc
chingfantsou <caojingfan@gmail.com>
2014-10-30 15:53:48 +0800

    revise the solution of prb_11, add two tests

--- solutions/list/prb_11.ml
+++ solutions/list/prb_11.ml
@@ -1,37 +1,33 @@
-(*   Modified run-length encoding.
-     Modify the result of the previous
-   problem in such a way that if an
-   element has no duplicates it is
-   simply copied into the result list.
-     Only elements with duplicates
-   are transferred as (N E) lists.
- *)
+(** Modified run-length encoding.
+    Modify the result of the previous
+    problem in such a way that if an
+    element has no duplicates it is
+    simply copied into the result list.
+    Only elements with duplicates
+    are transferred as (N E) lists. *)
 
-(*   Since OCaml lists are homogeneous,
-   one needs to define a type to hold both
-   single elements and sub-lists.
- *)
+(** Since OCaml lists are homogeneous,
+    one needs to define a type to hold both
+    single elements and sub-lists. *)
+
 type 'a rle_t =
   | One of 'a
   | Many of int * 'a
 
-let encode_m l =
-  let mdf pcg =
-    let Many (n,code) = pcg in
-    if n = 1 then
-      One code
-    else
-      pcg in
-  let rec ec_aux d_l pcg c_l =
-    match d_l with
-    | [] -> (mdf pcg)::c_l
-    | x :: t ->
-       let Many(n,y) = pcg in
-       if n = 0 || x = y then
-	 ec_aux t (Many(n+1,y)) c_l
-       else
-	 ec_aux t (Many(1,x)) ((mdf pcg)::c_l) in
-  if l = [] then
-    []
-  else
-    List.rev (ec_aux l (Many(0,List.hd l)) [])
+let uw = function
+  | Some a -> a
+  | None -> raise Not_found
+              
+let encode l =
+  let cur_rle, f_l =
+    List.fold_right (fun x par_en_l ->
+        let cur_rle, l = par_en_l in
+        let n, ele = cur_rle in
+        if (Some x) = ele then
+          (n+1, ele),l
+        else if n = 1 then
+          (1, Some x),((One  (uw ele)) :: l)
+        else
+          (1, Some x),((Many (n, uw ele)) :: l))
+      l ((0, None),[]) in
+  f_l
--- unit_test/list_test.ml
+++ unit_test/list_test.ml
@@ -1,35 +1,48 @@
 open OUnit2
+open List_prb
   
 let test01_nom test_ctxt = assert_equal
     (Some "d")
-    (List_prb.Prb_01.last [ "a" ; "b" ; "c" ; "d" ])
+    (Prb_01.last [ "a" ; "b" ; "c" ; "d" ])
 
 let test01_null test_ctxt = assert_equal
-    None (List_prb.Prb_01.last [])
+    None (Prb_01.last [])
+
+let test11_nom test_ctxt =
+  let open Prb_11 in
+  assert_equal
+    (encode
+       ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"])
+    [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
+     Many (4, "e")]
+
+let test11_null test_ctxt = assert_equal
+    (Prb_11.encode [])
+    []
 
 let test20_nom test_ctxt = assert_equal
     ["a"; "c"; "d"]
-    (List_prb.Prb_20.remove_at 1 ["a";"b";"c";"d"])
+    (Prb_20.remove_at 1 ["a";"b";"c";"d"])
 
 let test21_1 test_ctxt = assert_equal
-    (List_prb.Prb_21.insert_at "alfa" 1 ["a";"b";"c";"d"])
+    (Prb_21.insert_at "alfa" 1 ["a";"b";"c";"d"])
     ["a"; "alfa"; "b"; "c"; "d"]
 
 let test21_3 test_ctxt = assert_equal
-    (List_prb.Prb_21.insert_at "alfa" 3 ["a";"b";"c";"d"])
+    (Prb_21.insert_at "alfa" 3 ["a";"b";"c";"d"])
     ["a"; "b"; "c"; "alfa"; "d"]
 
 let test21_4 test_ctxt = assert_equal
-    (List_prb.Prb_21.insert_at "alfa" 4 ["a";"b";"c";"d"]) ["a"; "b"; "c"; "d"; "alfa"]
+    (Prb_21.insert_at "alfa" 4 ["a";"b";"c";"d"]) ["a"; "b"; "c"; "d"; "alfa"]
 
 let test22_asc test_ctxt = assert_equal
-    (List_prb.Prb_22.range 4 9) [4;5;6;7;8;9]
+    (Prb_22.range 4 9) [4;5;6;7;8;9]
 
 let test22_des test_ctxt = assert_equal
-    (List_prb.Prb_22.range 9 4) [9;8;7;6;5;4]
+    (Prb_22.range 9 4) [9;8;7;6;5;4]
 
 let test22_sgl test_ctxt = assert_equal
-    (List_prb.Prb_22.range 5 5) [5]
+    (Prb_22.range 5 5) [5]
 
 let suite =
   "list_test">:::
